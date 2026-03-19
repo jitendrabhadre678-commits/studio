640ffe27200f727b29d606a1678332c2bc7fc3c8
Jitendra Bhadre <jitendrabhadre11@gmail.com>
2026-03-19 06:18:07 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyD8OluRZFDfQLoa7TNUBlbJIzVcizBmcTs",
-  authDomain: "gameflashx-5c155.firebaseapp.com",
-  projectId: "gameflashx-5c155",
-  storageBucket: "gameflashx-5c155.firebasestorage.app",
-  messagingSenderId: "642888730380",
-  appId: "1:642888730380:web:df248255a7896500376b29",
-  measurementId: "G-VCN3SXSF98"
+  "projectId": "gameflashx-5c155",
+  "appId": "1:642888730380:web:b53abe8221e6f834376b29",
+  "apiKey": "AIzaSyD8OluRZFDfQLoa7TNUBlbJIzVcizBmcTs",
+  "authDomain": "gameflashx-5c155.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "642888730380"
 };
